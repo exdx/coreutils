32be417f0b81d322dbee70b2f7b01eed658c5e69
Denton24646 <Denton24646@gmail.com>
2022-06-30 08:30:55 -0400

    add colored crate to echo for colorized output

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use colored::Colorize;
 
 fn main() {
     let matches = App::new("echo")
@@ -21,5 +22,5 @@ fn main() {
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
+    print!("{}{}", text.join(" ").green(), if omit_newline {""} else {"\n"});
 }
