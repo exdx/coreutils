b9689a0f5cea71f7fabf6d77d92fa0cd933e9d08
Denton24646 <Denton24646@gmail.com>
2022-06-29 22:17:38 -0400

    init: echo

---
+++ README.md
@@ -0,0 +1,8 @@
+## Coreutils
+
+A rewrite of common UNIX utility programs in Rust.
+
+Based on the book Command-Line Rust by Ken Youens-Clark. 
+
+### Implemented
+[x] echo
---
+++ echo/tests/cli.rs
@@ -0,0 +1,57 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo")?;
+    cmd.arg("hello").assert().success();
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echo")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+
+
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    let args = &["Hello there"];
+    run(args, "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let args = &["Hello", "there"];
+    run(args, "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    let args = &["Hello  there", "-n"];
+    run(args, "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    let args = &["-n", "Hello", "there"];
+    run(args, "tests/expected/hello2.n.txt")
+}
