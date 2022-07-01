bac15017cd8b5cbe8c61ac14c4e4d6d6e446fe8b
Denton24646 <Denton24646@gmail.com>
2022-07-01 17:35:42 -0400

    cat: use color library from utils

--- cat/src/lib.rs
+++ cat/src/lib.rs
@@ -2,6 +2,8 @@ use clap::{App, Arg};
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::process;
+use utils;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,6 +12,7 @@ pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+    input_color: String,
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -37,16 +40,31 @@ pub fn get_args() -> MyResult<Config> {
                 .takes_value(false)
                 .help("Print only non-blank line numbers"),
         )
+        .arg(
+            Arg::with_name("color")
+                .short("c")
+                .help("Color formatting. Use lowercase, for example green")
+                .default_value(""),
+        )
         .get_matches();
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
         number_lines: matches.is_present("number"),
         number_nonblank_lines: matches.is_present("number-nonblank"),
+        input_color: matches.value_of("color").unwrap().to_string(),
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
+    let color = match utils::colors::get_color(&config.input_color) {
+        None => {
+            eprintln!("Error: unrecognized color selected: {}", config.input_color);
+            process::exit(1);
+        }
+        Some(color) => color,
+    };
+
     for filename in config.files {
         match open(&filename) {
             Err(e) => eprintln!("Failed to open {}: {}", filename, e),
@@ -54,6 +72,7 @@ pub fn run(config: Config) -> MyResult<()> {
                 let mut last_num = 0;
                 for (line_num, line_result) in file.lines().enumerate() {
                     let line = line_result?;
+                    let line = utils::colors::build_colored_string(&line, color);
                     if config.number_lines {
                         println! {"{:>6}\t{}", line_num + 1, line}
                     } else if config.number_nonblank_lines {
