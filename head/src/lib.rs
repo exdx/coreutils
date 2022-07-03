55c08bf9030489570cdca8629debfb1d3a52e54f
Denton24646 <Denton24646@gmail.com>
2022-07-02 23:12:39 -0400

    head: add usize parsing

--- head/src/lib.rs
+++ head/src/lib.rs
@@ -15,22 +15,27 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .about("head UNIX tool")
         .arg(
-            Arg::with_name("files")
-                .value_name("FILES")
-                .default_value("-")
-                .multiple(true)
-                .help("File(s) to read from"),
-        )
-        .arg(
             Arg::with_name("line_count")
-                .value_name("LINE COUNT")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
                 .default_value("10")
                 .help("How many lines to display"),
         )
         .arg(
             Arg::with_name("chars")
-                .value_name("CHARS")
-                .help("How many characters to display"),
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("How many characters to display")
+                .conflicts_with("lines"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .default_value("-")
+                .multiple(true)
+                .help("File(s) to read from"),
         )
         .get_matches();
 
@@ -46,4 +51,26 @@ pub fn get_args() -> MyResult<Config> {
 pub fn run(config: Config) -> MyResult<()> {
     dbg!(config);
     Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse::<usize>() {
+        Ok(u) if u > 0 => Ok(u),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string())
 }
